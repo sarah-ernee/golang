a054793f80dc10ad4e0ea0b18c04aa9ee79b4aef
Sarah Ng <93563644+sarah-ernee@users.noreply.github.com>
2024-03-06 09:53:52 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+func main() {
+
+	// shorthand inside function for var declaration
+	// var := value
+
+	// strings
+	var nameOne string = "mario"
+	fmt.Println(nameOne)
+
+	// ints
+	ageOne := 20
+	fmt.Println(nameOne, "is", ageOne)
+
+}
